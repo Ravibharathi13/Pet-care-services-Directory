8bbb2a4d47abae35f9c0f5869c21d8b861e134db
Ravibharathi13 <155134424+Ravibharathi13@users.noreply.github.com>
2025-10-06 19:45:22 +0530

    Update AdminLogin.jsx

--- client/src/pages/Admin/AdminLogin.jsx
+++ client/src/pages/Admin/AdminLogin.jsx
@@ -33,7 +33,7 @@ export default function AdminLogin() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/auth/login', {
+      const response = await fetch('https://pet-care-services-directory.onrender.com/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
