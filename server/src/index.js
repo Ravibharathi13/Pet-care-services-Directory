85d4513d9057150c3c5d6ed2bc51670e1c6f6d1d
Ravibharathi13 <155134424+Ravibharathi13@users.noreply.github.com>
2025-11-20 21:49:44 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "https://pet-care-services-directory-client.onrender.com",
   credentials: true
 }));
 app.use(cookieParser());
