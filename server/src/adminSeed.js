e7e870d7f44af44fe3e8223717cf0ff12de4e0f5
Ravibharathi13 <155134424+Ravibharathi13@users.noreply.github.com>
2025-11-21 06:04:36 +0530

    Update MongoDB URI for admin seeding

--- server/src/adminSeed.js
+++ server/src/adminSeed.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import Admin from "./models/Admin.js";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/petcare";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://ravibharathi:Ravi2011@cluster0.lprr79i.mongodb.net/petcare";
 
 async function seedAdmin() {
   try {
